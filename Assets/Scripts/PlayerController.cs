fe24af74036b066565dbb5a6de2b6e40f4e5f1f3
Seb47536 <a47536@alunos.ipb.pt>
2026-03-30 19:59:08 +0100

    Update PlayerController.cs
    3. Test the default 2D Beginner movement
    
    4. Adjust the movement to units per second
    
    5. Adjust the speed of movement
    
    6. Check your script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,6 @@ public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-       
         MoveAction.Enable();
     }
 
@@ -19,7 +18,7 @@ public InputAction MoveAction;
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
 
         
