a3c08172984255f223fccc9844d7dffd961b8a55
Seb47536 <a47536@alunos.ipb.pt>
2026-03-31 01:07:23 +0100

    Movement Fix
    6. Fix the player character rotation
    8. Fix the PlayerCharacter jittering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +8,25 @@ public class PlayerController : MonoBehaviour
 
 {
 public InputAction MoveAction;
+private Rigidbody2D rigidbody2d;
+private Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+    }
 
-        
-        
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
